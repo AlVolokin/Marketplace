d580e2c9c04a7b431ef057e3c0bf267cdbc84f1c
Nikola Nikolov <1nikola.r.nikolov@gmail.com>
2018-03-26 22:40:44 +0300

    Update TestMarketplace.sol

--- TestMarketplace.sol
+++ TestMarketplace.sol
@@ -1,3 +1,4 @@
+pragma solidity 0.4.19;
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/Marketplace.sol";
@@ -12,9 +13,9 @@ contract TestMarketplace {
         uint newProductQuantity = 10;
         bytes32 newProductID = marketplace.newProduct("jacket", 500000000, 10);
         
-        Assert.equal(marketplace.getProduct.products[newProductID].name, newProductName, "The corresponding name should be jacket");
-        Assert.equal(marketplace.getProduct.products[newProductID].price, newProductPrice, "The corresponding price should be 500000000");
-        Assert.equal(marketplace.getProduct.products[newProductID].quantity, newProductQuantity, "The corresponding quantity should be 10");
+        Assert.equal(marketplace.products[newProductID].name, newProductName, "The corresponding name should be jacket");
+        Assert.equal(marketplace.products[newProductID].price, newProductPrice, "The corresponding price should be 500000000");
+        Assert.equal(marketplace.products[newProductID].quantity, newProductQuantity, "The corresponding quantity should be 10");
     }
     
     function testUpdatedProduct(){
@@ -28,6 +29,7 @@ contract TestMarketplace {
         
         marketplace.update(newProductID, updatedQuantity);
         
-        Assert.equal(marketplace.getProduct.products[newProductID].quantity, updatedQuantity, "The corresponding quantity should be 20");
+        Assert.equal(marketplace.products[newProductID].quantity, updatedQuantity, "The corresponding quantity should be 20");
     }
 }
+
