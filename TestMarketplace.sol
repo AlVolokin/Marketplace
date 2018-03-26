b432b16562893d8092599ed35b6001537342c9c8
Nikola Nikolov <1nikola.r.nikolov@gmail.com>
2018-03-26 16:26:25 +0300

    Update TestMarketplace.sol

--- TestMarketplace.sol
+++ TestMarketplace.sol
@@ -16,4 +16,18 @@ contract TestMarketplace {
         Assert.equal(marketplace.getProduct.products[newProductID].price, newProductPrice, "The corresponding price should be 500000000");
         Assert.equal(marketplace.getProduct.products[newProductID].quantity, newProductQuantity, "The corresponding quantity should be 10");
     }
+    
+    function testUpdatedProduct(){
+        Marketplace marketplace = new Marketplace();
+        
+        string newProductName = "jacket"; 
+        uint newProductPrice = 500000000; 
+        uint newProductQuantity = 10;
+        uint updatedQuantity = 20;
+        bytes32 newProductID = marketplace.newProduct("jacket", 500000000, 10);
+        
+        marketplace.update(newProductID, updatedQuantity);
+        
+        Assert.equal(marketplace.getProduct.products[newProductID].quantity, updatedQuantity, "The corresponding quantity should be 20");
+    }
 }
